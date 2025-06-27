b0394de4e6e0a24c37f203730d0b6cdb0dd80360
Arjun kothanath <arjunmkothanath@gmail.com>
2025-06-27 10:24:03 +0530

    Delete New folder directory

--- New folder/admins/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from general.models import Feedback
-
-admin.site.unregister(Feedback)
-
-@admin.register(Feedback)
-class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ('user', 'feedback_type', 'location', 'message', 'status', 'created_at')
